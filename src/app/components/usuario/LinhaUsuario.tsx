305a96fc454919042a20c036e31dee3e0f863a5d
Luiz Gustavo Lourenço Moura <lglmoura@gmail.com>
2024-09-30 16:00:22 -0300

    refactore: linha usuario

--- src/app/components/usuario/LinhaUsuario.tsx
+++ src/app/components/usuario/LinhaUsuario.tsx
@@ -1,5 +1,5 @@
 import { Usuario } from '@/core/model/Usuario'
-import Image from 'next/image'
+//import Image from 'next/image'
 
 export interface LinhaUsuarioProps {
     usuario: Usuario
@@ -12,13 +12,7 @@ export default function LinhaUsuario(props: LinhaUsuarioProps) {
             className="flex bg-zinc-900 items-center gap-5 p-4 rounded-md cursor-pointer"
             onClick={() => props.onClick?.(props.usuario)}
         >
-            <Image
-                src="http://source.unsplash.com/random/80x80?avatar"
-                width={80}
-                height={80}
-                className="rounded-full"
-                alt="Avatar"
-            />
+            
             <div className="flex flex-col">
                 <span className="text-xl font-black">{props.usuario.nome}</span>
                 <span className="text-sm text-zinc-400">{props.usuario.email}</span>
